a4f84713188aeaab9b9833b981dcb71a6be9f8e4
Aman Yadav <58115487+AmanYadav007@users.noreply.github.com>
2021-10-26 14:52:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include 'includes/common.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Sparks Bank</title>
+</head>
+
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="content">
+    <div class="main-box">
+        <h1>New User</h1>
+        <p>Click the button below to register</p>
+        <button type="submit" class="btn"><a href="user.php" style="text-decoration:none; color: white">Register</a></button>
+    </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
